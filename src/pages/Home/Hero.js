b43ed61341d3ebdd6c1e441a05f3969c06569efb
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2023-05-15 00:43:14 +0600

    hero section added

--- src/pages/Home/Hero.js
+++ src/pages/Home/Hero.js
@@ -1,23 +1,35 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
   return (
     <div
       className="hero min-h-screen"
       style={{
-        backgroundImage: `url("/images/stock/photo-1507358522600-9f71e620c44e.jpg")`,
+        backgroundImage: `url("https://www.capetownmagazine.com/media_lib/preview/226be2bc923500290cd9505dac304c9f.preview.jpg")`,
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">
+            <Typewriter
+              options={{
+                strings: ["Welcome To cafe-BISTRO"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Welcome to our cozy coffee shop, where the aroma of freshly brewed
+            coffee fills the air and the warm ambiance invites you to relax and
+            unwind. Our passion for coffee is reflected in every cup we serve,
+            using only the highest quality beans and carefully crafted
+            techniques to bring out the rich flavors and aromas that you know
+            and love.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn bg-amber-950">Explore Our Menu</button>
         </div>
       </div>
     </div>
